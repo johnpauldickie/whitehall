546b31816a5e78841ea274d6f06b39c632281107
Bevan Loon <bevan.loon@digital.cabinet-office.gov.uk>
2019-01-28 12:43:15 +0000

    Improve SQL on admin dashboard
    The admin dashboard lists, for a user, their organisation's force
    published documents. Currently this is done through an ActiveRecord
    query
    current_user.organisation.editions.force_published.includes(:translations,
    :versions).in_reverse_chronological_order.limit(5). One of The resulting queries
    is (edited down a bit and in pseudocode):
    
    ```
    SELECT editions.* FROM editions WHERE editions.id IN (user's org's force
    published edition ids)
    ```
    
    The subquery returning the user's organisation's force published edition
    on the development VM for GDS is returing 7360ish ids, though that
    contains duplicates and actually only represents 1630ish unique ids.
    As far as I can tell this is because there is a LEFT OUTER join between
    editions and edition_translations that AR is generating because of the
    "includes" we have, though I haven't analysed that in great detail.
    
    None the less, the queries seem rather inneficient to me and I think
    we can do better with some hand crafted sql.

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,7 +2,12 @@ class Admin::DashboardController < Admin::BaseController
   def index
     if current_user.organisation
       @draft_documents = Edition.authored_by(current_user).where(state: 'draft').includes(:translations, :versions).in_reverse_chronological_order
-      @force_published_documents = current_user.organisation.editions.force_published.includes(:translations, :versions).in_reverse_chronological_order.limit(5)
+
+      @force_published_documents = Edition.
+        joins("INNER JOIN edition_organisations on editions.id = edition_organisations.id INNER JOIN edition_translations on edition_translations.id = editions.id").
+        where("edition_organisations.organisation_id = ?", current_user.organisation.id).
+        order("editions.public_timestamp desc, editions.document_id desc, editions.id desc").
+        limit(5)
     end
   end
 end
