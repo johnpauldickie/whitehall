cd42cf362dcbea19ab7b363e4302400454edd58a
William Franklin <william.franklin@digital.cabinet-office.gov.uk>
2019-04-08 15:18:32 +0100

    Use Locale.current.english?
    This is better than using a custom method for checking if the locale is english.

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -9,7 +9,7 @@ class AnnouncementsController < DocumentsController
 
     respond_to do |format|
       format.html do
-        return redirect_to_news_and_communications if is_english_locale?
+        return redirect_to_news_and_communications if Locale.current.english?
 
         @content_item = Whitehall
           .content_store
@@ -35,12 +35,6 @@ private
 
   def scheduled_announcements
     Announcement.scheduled.order("scheduled_publication asc")
-  end
-
-  def is_english_locale?
-    # We only want to redirect English locale requests for now,
-    # as translations for this finder haven't been moved to finder-frontend yet.
-    params[:locale].nil?
   end
 
   def redirect_to_news_and_communications
